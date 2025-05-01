401037fb361e082f09e1aa5bc7cea2babe8dc111
Umair505 <moinuli359@gmail.com>
2025-05-01 22:40:35 +0600

    data type

---
+++ Module 01/typscript_technocrat/module1/src/1.4.ts
@@ -0,0 +1,27 @@
+//Basic data type 
+//string 
+//Explicit data type jokhn amra declare kore dibo type
+let firstName: string = 'Moinul'
+let LastName = 'Islam'//Implicit data type jeita type script nije dharona kore nibe
+//Number
+let roll: number = 123
+//Boolean
+let isAdmin: boolean = true;
+//undefined
+let x : undefined = undefined;
+//null
+let y: null = null;
+
+let d;//any type jeita use kora uchit na
+d = 123;
+d= 'jlksjdf'
+d= true;
+let a : number;
+
+//Non primitive
+let friends = ['satyam','saima']
+let eligibleRollLlist  : number[] = [1,2,3]
+
+//tuple --> array --> order -->type of values 
+let coordinates:[number,number] = [1,5]
+let ageName :[number,string,boolean] = [24,"Moinul",false]
